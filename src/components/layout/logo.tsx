2f4c52e04e5993a71c6d10a5afde0b5fe659b14d
AL <ArnasLebedevas@gmail.com>
2025-08-11 21:16:54 +0300

    improve logo, styles

--- src/components/layout/logo.tsx
+++ src/components/layout/logo.tsx
@@ -1,12 +1,11 @@
 import { content } from "@/constants/content";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Logo() {
   return (
-    <Link href={content.navigation.home.path}>
-      <h1 className="text-5xl font-semibold">
-        AL<span className="text-accent">.</span>
-      </h1>
+    <Link href={content.navigation.home.path} className="flex items-center justify-center">
+      <Image src="/logo.svg" alt="AL Logo" width={54} height={54} priority />
     </Link>
   );
 }
