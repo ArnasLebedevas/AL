672f000d2e6d9d3c0ed43b772ac02cad26353c46
AL <ArnasLebedevas@gmail.com>
2025-08-18 22:19:46 +0300

    hexagon loader

--- src/features/transitions/components/hexagon-loader.tsx
+++ src/features/transitions/components/hexagon-loader.tsx
@@ -4,6 +4,7 @@ import { AnimatePresence, motion } from "framer-motion";
 import { useState } from "react";
 import { hexagonMotionConfig } from "../config/motion";
 import Logo from "@/shared/icons/logo";
+import { usePathname } from "next/navigation";
 
 interface Props {
   children: React.ReactNode;
@@ -11,28 +12,23 @@ interface Props {
 
 export default function HexagonLoader({ children }: Props) {
   const [isAnimating, setIsAnimating] = useState(true);
-  const [hasLoaderExited, setHasLoaderExited] = useState(false);
-
+  const pathname = usePathname();
+  
   return (
-    <div className="relative">
-      <AnimatePresence
-        mode="wait"
-        onExitComplete={() => setHasLoaderExited(true)}
-      >
-        {isAnimating && (
-          <motion.div
-            className="flex items-center justify-center h-screen w-screen overflow-hidden bg-[var(--primary)] fixed top-0 left-0"
-            {...hexagonMotionConfig}
-          >
-            <Logo
-              animated
-              onAnimationComplete={() => setIsAnimating(false)}
-              className="h-16 w-16 xl:h-20 xl:w-20"
-            />
-          </motion.div>
-        )}
-      </AnimatePresence>
-      {hasLoaderExited && children}
-    </div>
+    <AnimatePresence mode="wait">
+      {isAnimating && (
+        <motion.div
+          className="flex items-center justify-center h-screen w-screen overflow-hidden bg-[var(--primary)] fixed top-0 left-0 z-10"
+          {...hexagonMotionConfig}
+        >
+          <Logo
+            animated
+            onAnimationComplete={() => setIsAnimating(false)}
+            className="h-16 w-16 xl:h-20 xl:w-20"
+          />
+        </motion.div>
+      )}
+      <div key={pathname}>{children}</div>
+    </AnimatePresence>
   );
 }
